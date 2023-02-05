c65c8ed67eca8f004777f8cc9fef3d39c8fd53af
Ernest <ernestasena@gmail.com>
2023-02-05 17:12:31 +0300

    add progress bar with tqdm

--- 100 Days of Code Challenge/Day 005 (Python Loops)/1.Using the for loop with Python Lists.py
+++ 100 Days of Code Challenge/Day 005 (Python Loops)/1.Using the for loop with Python Lists.py
@@ -1,6 +1,13 @@
 # for loops are used to repeat a task for as many times as specified based. Example;
 # we will print out the value of fruit for each iteration in the following list
 fruits = ['apple', 'peach', 'pear']
+from tqdm import tqdm
+for fruit in tqdm(fruits):
+    for fruit in tqdm(fruits):
+        for fruit in tqdm(fruits):
+            for fruit in tqdm(fruits):
+                for fruit in tqdm(fruits):
+                    print(fruit)
+    
 
-for fruit in fruits:
-    print(fruit)
+
