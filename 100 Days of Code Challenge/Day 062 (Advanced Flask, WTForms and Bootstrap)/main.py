647ea6c6dbc09eb28ce8ddbedaa1a4de0780c624
Ernest <ernestasena@gmail.com>
2023-02-24 22:21:14 +0300

    add docstring

--- 100 Days of Code Challenge/Day 062 (Advanced Flask, WTForms and Bootstrap)/main.py
+++ 100 Days of Code Challenge/Day 062 (Advanced Flask, WTForms and Bootstrap)/main.py
@@ -47,6 +47,7 @@ def add_cafe():
 
 @app.route('/cafes')
 def cafes():
+    """Render website's cafes page."""
     with open('cafe-data.csv', newline='') as csv_file:
         csv_data = csv.reader(csv_file, delimiter=',')
         list_of_rows = []
