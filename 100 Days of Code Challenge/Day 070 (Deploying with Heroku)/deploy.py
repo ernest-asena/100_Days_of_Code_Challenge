851427f37cfa57c9a514d5c2f0eae55a732372fc
Ernest <ernestasena@gmail.com>
2023-02-25 23:29:43 +0300

    note on heroku

--- 100 Days of Code Challenge/Day 070 (Deploying with Heroku)/deploy.py
+++ 100 Days of Code Challenge/Day 070 (Deploying with Heroku)/deploy.py
@@ -1,6 +1,8 @@
 # Today, I deployed my portfolio webpage with heroku
 # here is a link to it; https://ernest-asena.herokuapp.com
 
+# Note from the future: Heroku took down their free dynos, so I will have to find another way to deploy my portfolio webpage
+
 
 
 
