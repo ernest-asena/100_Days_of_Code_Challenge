8d383f20c769e6898a1a2802c0eccd71c3aab8af
Ernest <ernestasena@gmail.com>
2023-02-26 23:24:24 +0300

    refactor code

--- 100 Days of Code Challenge/Day 029 (Password Manager)/main.py
+++ 100 Days of Code Challenge/Day 029 (Password Manager)/main.py
@@ -8,6 +8,7 @@ from Clearscreen import clear
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_pass():
+    """Generates a random password using letters, numbers and symbols."""
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
                'v',
                'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
