134e1ca0a446cea60a1ea56be1a8cc417178ed57
Ernest <ernestasena@gmail.com>
2023-03-04 21:34:31 +0300

    add docstring

--- 100 Days of Code Challenge/Day 067 (Blog Capstone Project Part 3_ RESTFUL Routing)/main.py
+++ 100 Days of Code Challenge/Day 067 (Blog Capstone Project Part 3_ RESTFUL Routing)/main.py
@@ -21,6 +21,7 @@ db = SQLAlchemy(app)
 
 # CONFIGURE TABLE
 class BlogPost(db.Model):
+    """Create a blog post table"""
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
     subtitle = db.Column(db.String(250), nullable=False)
