76babec3ed96ea32210ba1634b37790e267408b6
Ernest <ernestasena@gmail.com>
2023-03-04 21:41:26 +0300

    add docstring

--- 100 Days of Code Challenge/Day 068 (Authentication with Flask)/main.py
+++ 100 Days of Code Challenge/Day 068 (Authentication with Flask)/main.py
@@ -15,6 +15,7 @@ login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
+    """Create user loader function for flask-login"""
     return User.query.get(int(user_id))
 
 
