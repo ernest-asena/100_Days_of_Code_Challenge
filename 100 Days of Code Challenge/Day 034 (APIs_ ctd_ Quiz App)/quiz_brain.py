30bc369e0df160c195f44125d81b5e8ce95180e6
Ernest <ernestasena@gmail.com>
2023-03-07 21:13:35 +0300

    add documentation

--- 100 Days of Code Challenge/Day 034 (APIs_ ctd_ Quiz App)/quiz_brain.py
+++ 100 Days of Code Challenge/Day 034 (APIs_ ctd_ Quiz App)/quiz_brain.py
@@ -2,7 +2,7 @@ import html
 
 
 class QuizBrain:
-
+    """This class is responsible for keeping track of the score and the question number."""
     def __init__(self, q_list):
         self.question_number = 0
         self.score = 0
