27c0dd136d9294041e5858205a2b88ab9cdb5fc2
Ernest <ernestasena@gmail.com>
2023-03-08 23:56:31 +0300

    add documentation

--- 100 Days of Code Challenge/Day 034 (APIs_ ctd_ Quiz App)/question_model.py
+++ 100 Days of Code Challenge/Day 034 (APIs_ ctd_ Quiz App)/question_model.py
@@ -1,5 +1,6 @@
 class Question:
     """This class represents a question and its answer."""
     def __init__(self, q_text, q_answer):
+        """initialize the question and answer attributes."""
         self.text = q_text
         self.answer = q_answer
