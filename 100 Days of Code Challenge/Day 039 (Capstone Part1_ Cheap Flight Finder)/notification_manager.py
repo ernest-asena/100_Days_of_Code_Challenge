fe6f24abe0e39523bdbaca332848b58dc5012f40
Ernest <ernestasena@gmail.com>
2023-03-09 23:29:32 +0300

    add documentation

--- 100 Days of Code Challenge/Day 039 (Capstone Part1_ Cheap Flight Finder)/notification_manager.py
+++ 100 Days of Code Challenge/Day 039 (Capstone Part1_ Cheap Flight Finder)/notification_manager.py
@@ -10,6 +10,7 @@ TWILIO_VERIFIED_NUMBER = config.TWILIO_VERIFIED_PHONE_TWO
 class NotificationManager:
     """This class is responsible for sending notifications with the deal flight details."""
     def __init__(self):
+        """Initialize the class with Twilio client."""
         self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
 
     def send_sms(self, message):
