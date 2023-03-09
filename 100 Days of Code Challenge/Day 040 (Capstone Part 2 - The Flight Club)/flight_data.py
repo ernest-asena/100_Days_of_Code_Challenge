3eeb3c64f0e7aa4ecc8bb6a764f2f7ca3ef590ee
Ernest <ernestasena@gmail.com>
2023-03-09 23:31:31 +0300

    add documentation

--- 100 Days of Code Challenge/Day 040 (Capstone Part 2 - The Flight Club)/flight_data.py
+++ 100 Days of Code Challenge/Day 040 (Capstone Part 2 - The Flight Club)/flight_data.py
@@ -1,5 +1,5 @@
 class FlightData:
-
+    """This class is responsible for structuring the flight data."""
     def __init__(
             self, price, origin_city, origin_airport, destination_city, destination_airport, out_date, return_date,
             stop_overs=0, via_city=""):
