ecf3067855fa2e25bc21d1efb5ac353d65bc4986
Ernest <ernestasena@gmail.com>
2023-03-12 21:27:35 +0300

    add docstring

--- 100 Days of Code Challenge/Day 020 (OOP concepts to build the famous snake game)/snake.py
+++ 100 Days of Code Challenge/Day 020 (OOP concepts to build the famous snake game)/snake.py
@@ -48,5 +48,6 @@ class Snake:
             self.head.setheading(LEFT)
 
     def right(self):
+        """Moves the snake right"""
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
