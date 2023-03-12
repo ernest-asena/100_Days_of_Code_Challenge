d6eeda51cc03beaf9ac5dacb8cfdd004295cc85a
Ernest <ernestasena@gmail.com>
2023-03-12 21:21:34 +0300

    add docstring

--- 100 Days of Code Challenge/Day 019 (Event Listener, Higher Order Functions, State and Multiple Instances)/Make an Etch-A-Sketch App.py
+++ 100 Days of Code Challenge/Day 019 (Event Listener, Higher Order Functions, State and Multiple Instances)/Make an Etch-A-Sketch App.py
@@ -12,6 +12,7 @@ def right():
     """turn the turtle right by 10 degrees"""
     tim.right(10)
 def left():
+    """turn the turtle left by 10 degrees"""
     tim.left(10)
 def clear():
     tim.clear()
