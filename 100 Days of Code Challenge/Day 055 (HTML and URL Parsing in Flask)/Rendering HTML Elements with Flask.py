801b660c725327b418ac68896d7fdb586fd6e15e
Ernest <ernestasena@gmail.com>
2023-03-23 21:48:38 +0300

    add documentation

--- 100 Days of Code Challenge/Day 055 (HTML and URL Parsing in Flask)/Rendering HTML Elements with Flask.py
+++ 100 Days of Code Challenge/Day 055 (HTML and URL Parsing in Flask)/Rendering HTML Elements with Flask.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    """This is the home page"""
     return '<h1>Hello World</h1><p>This is a simple paragraph to say that we are happy with Flask.<br> Laca is actually ' \
            'dancing just to prove it. Trust Laca!<p>' \
            '<img src="https://media.giphy.com/media/ORVArZlFq0DSyVJ6C8/giphy.gif">'
