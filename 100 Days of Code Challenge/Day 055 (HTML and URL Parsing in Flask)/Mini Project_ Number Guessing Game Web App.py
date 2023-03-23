830be83e33253bee9ddf28ac5faa53120187c1c9
Ernest <ernestasena@gmail.com>
2023-03-23 21:43:15 +0300

    add documentation

--- 100 Days of Code Challenge/Day 055 (HTML and URL Parsing in Flask)/Mini Project_ Number Guessing Game Web App.py
+++ 100 Days of Code Challenge/Day 055 (HTML and URL Parsing in Flask)/Mini Project_ Number Guessing Game Web App.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    """This is the home page"""
     return "<h1>Guess a number between 0 and 9</h1>" \
            "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'/>"
 
