01793b8951e293bfd4bed427b5dec7356c96c3c1
Ernest <ernestasena@gmail.com>
2023-04-02 21:56:36 +0300

    introduction to variables

---
+++ Day001-Working with Variables in Python to Manage Data/6.Python variables.py
@@ -0,0 +1,11 @@
+"""
+A Python variable is a symbolic name that is a reference or pointer to an object. 
+Once an object is assigned to a variable, you can refer to the object by that name. 
+But the data itself is still contained within the object. 
+For example:
+ >>> >>> n = 300.
+"""
+
+name = input("What is your name:: ")
+
+city = "Nairobi"
