dfd919116097042a0dcef94ca66105fa87a0270b
Ernest <ernestasena@gmail.com>
2023-04-02 21:45:54 +0300

    inputs exercise and the len() function

---
+++ Day001-Working with Variables in Python to Manage Data/5.Inputs exercise.py
@@ -0,0 +1,12 @@
+# Instructions
+# Write a program that prints the number of characters in a user's name. You might need to Google for a function that calculates the length of a string.
+
+user_name = input("Enter your name:: ")
+
+no_of_chars = len(user_name)
+
+print(no_of_chars)
+
+# OR JUST DO:
+
+print(len(input("What is your name?:: ")))
