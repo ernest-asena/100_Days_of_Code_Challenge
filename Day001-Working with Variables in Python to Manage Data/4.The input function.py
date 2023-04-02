916c1f0603a2a8741a1c450d2ae7c00f25aa757b
Ernest <ernestasena@gmail.com>
2023-04-02 21:39:49 +0300

    the input() function

---
+++ Day001-Working with Variables in Python to Manage Data/4.The input function.py
@@ -0,0 +1,8 @@
+# What if we wanted to enter some data? eg say ask the user for their name?
+
+# input('prompt to user'), for example:
+input("What is your name?")
+
+# We can take this input and use it in the program.
+
+print("Hello " + input("What is your name?:"))
