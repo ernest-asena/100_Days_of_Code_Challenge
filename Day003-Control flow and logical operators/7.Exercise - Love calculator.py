a847e3d0e8f1be29ffa85d6193797afb6f617131
Ernest <ernestasena@gmail.com>
2023-04-06 23:44:33 +0300

    edit welcome message

--- Day003-Control flow and logical operators/7.Exercise - Love calculator.py
+++ Day003-Control flow and logical operators/7.Exercise - Love calculator.py
@@ -22,7 +22,7 @@
 # Example Output
 # Your final bill is: $28.
 
-print("Welcome to Python Pizza Deliveries!")
+print("Hi. Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M, or L ")
 add_pepperoni = input("Do you want pepperoni? Y or N ")
 extra_cheese = input("Do you want extra cheese? Y or N ")
