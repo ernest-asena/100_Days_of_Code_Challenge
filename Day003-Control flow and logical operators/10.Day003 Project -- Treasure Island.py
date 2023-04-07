6e077e624fd576bdbd4dbfacd7fe1f8727fbf719
Ernest <ernestasena@gmail.com>
2023-04-07 08:37:39 +0300

    refactor code

--- Day003-Control flow and logical operators/10.Day003 Project -- Treasure Island.py
+++ Day003-Control flow and logical operators/10.Day003 Project -- Treasure Island.py
@@ -28,7 +28,8 @@ print("Your mission is to find the treasure.")
 choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
 if choice1 == "left":
     choice2 = input(
-        'You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
+        'You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type '
+        '"swim" to swim across. \n').lower()
     if choice2 == "wait":
         choice3 = input(
             "You now arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. "
