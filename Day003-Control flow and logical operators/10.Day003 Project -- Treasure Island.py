542b7e942b805aa5305619d62d66549e0f0ec68d
Ernest <ernestasena@gmail.com>
2023-04-07 08:37:30 +0300

    refactor code

--- Day003-Control flow and logical operators/10.Day003 Project -- Treasure Island.py
+++ Day003-Control flow and logical operators/10.Day003 Project -- Treasure Island.py
@@ -31,7 +31,8 @@ if choice1 == "left":
         'You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
     if choice2 == "wait":
         choice3 = input(
-            "You now arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
+            "You now arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. "
+            "Which colour do you choose? \n").lower()
         if choice3 == "red":
             print("It's a room full of fire. Game Over!!!")
         elif choice3 == "yellow":
