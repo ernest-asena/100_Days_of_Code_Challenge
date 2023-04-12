dc9db68476901b1b6a3508bf3b19d15cedf68bd4
Ernest <ernestasena@gmail.com>
2023-04-12 19:56:22 +0300

    random module ii

--- Day004-Randomisation and Python Lists/1.Random Module.py
+++ Day004-Randomisation and Python Lists/1.Random Module.py
@@ -3,3 +3,10 @@ import random
 some_integer = random.randint(100, 200)
 
 print(some_integer)
+
+random_float = random.random()
+
+print(random_float)
+
+# Make it a whole number
+print(random_float * 5)
