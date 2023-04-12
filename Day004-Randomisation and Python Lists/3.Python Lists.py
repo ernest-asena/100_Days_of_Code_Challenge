af114a482a1ed85e634a7ca5c5076e3570d37f9e
Ernest <ernestasena@gmail.com>
2023-04-12 20:05:52 +0300

    Python lists ii

---
+++ Day004-Randomisation and Python Lists/3.Python Lists.py
@@ -0,0 +1,12 @@
+# In Python, a list is an ordered collection of elements, which can be of any data type (e.g., strings, integers, floats, etc.). 
+# Lists are one of the most versatile data structures in Python, and they are widely used in many programming tasks.
+
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+print(states_of_america)
+
+print(states_of_america[0])
+print(states_of_america[-1])
+
+
+dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
