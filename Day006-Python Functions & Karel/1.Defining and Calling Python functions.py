6e66e6f18dbbc786862ad1336282b6f91453c026
Ernest <ernestasena@gmail.com>
2023-04-14 17:24:39 +0300

    defining functions

--- Day006-Python Functions & Karel/1.Defining and Calling Python functions.py
+++ Day006-Python Functions & Karel/1.Defining and Calling Python functions.py
@@ -1,2 +1,9 @@
 # In Python, a function is a block of organized and reusable code that performs a specific task.
 # Functions help break down large programs into smaller, modular parts, making them easier to read, debug, and maintain.
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+
+my_function()
