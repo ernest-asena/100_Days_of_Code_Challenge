c73fac8f7a76e5f814d0b7bf9ed2f174a3cea2b5
Ernest <ernestasena@gmail.com>
2023-04-14 17:23:24 +0300

    functions start

---
+++ Day006-Python Functions & Karel/1.Defining and Calling Python functions.py
@@ -0,0 +1,2 @@
+# In Python, a function is a block of organized and reusable code that performs a specific task.
+# Functions help break down large programs into smaller, modular parts, making them easier to read, debug, and maintain.
