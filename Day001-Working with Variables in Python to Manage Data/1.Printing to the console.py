731b8d404d53c39594230ebbdd05ace8a120c47a
Ernest <ernestasena@gmail.com>
2023-04-16 15:38:39 +0300

    remove all files

--- Day001-Working with Variables in Python to Manage Data/1.Printing to the console.py
+++
@@ -1,5 +0,0 @@
-#using the print function to print to the console
-
-print("Day 1 - Python Print Function")
-print("The function is declared like this:")
-print("print('what to print')")
--- Day001-Working with Variables in Python to Manage Data/4.The input function.py
+++
@@ -1,8 +0,0 @@
-# What if we wanted to enter some data? eg say ask the user for their name?
-
-# input('prompt to user'), for example:
-input("What is your name?")
-
-# We can take this input and use it in the program.
-
-print("Hello " + input("What is your name?:"))
