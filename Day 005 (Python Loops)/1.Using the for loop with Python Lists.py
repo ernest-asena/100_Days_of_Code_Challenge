ac42c49b20dafffb1536caff332c641acd21668f
Ernest <ernestasena@gmail.com>
2023-09-01 20:49:43 +0300

    loops

--- Day 005 (Python Loops)/1.Using the for loop with Python Lists.py
+++ Day 005 (Python Loops)/1.Using the for loop with Python Lists.py
@@ -1,6 +1,6 @@
 # for loops are used to repeat a task for as many times as specified based. Example;
 # we will print out the value of fruit for each iteration in the following list
-fruits = ['apple', 'peach', 'pear']
+fruits = ['apple', 'peach', 'pear', 'banana']
 
 for fruit in fruits:
     print(fruit)
