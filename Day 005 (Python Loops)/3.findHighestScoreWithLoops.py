d0450a65977ab40d38ed42069f16052f280220cb
Ernest <ernestasena@gmail.com>
2023-09-01 20:51:20 +0300

    loops ii

--- Day 005 (Python Loops)/3.findHighestScoreWithLoops.py
+++ Day 005 (Python Loops)/3.findHighestScoreWithLoops.py
@@ -6,7 +6,7 @@
 # 
 # The highest score in the class is: x
 
-student_scores = input("Input a list of student scores:\nList Here:   ").split()
+student_scores = input("Input a list of student scores:\nList Here::   ").split()
 for n in range(0, len(student_scores)):
     student_scores[n] = int(student_scores[n])
 print(student_scores)
